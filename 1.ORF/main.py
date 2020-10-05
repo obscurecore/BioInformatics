8deafa9583a97c0109016627c717bb5f92311ca6
Ruslan Potapov <pruslang@gmail.com>
2020-10-05 17:11:19 +0300

    feature

--- 1.ORF/main.py
+++ 1.ORF/main.py
@@ -1,5 +1,27 @@
 import numpy as np
 from Bio.Seq import Seq
+import regex as re
+
+
+def find_largest_polypeptide_in_DNA(seq, translationTable=1):
+    allPossibilities = []
+    for frame in range(3):
+        print("CHECK OUT")
+        print(str(Seq(seq[frame:]).translate(translationTable)))
+        print("STOP")
+    """
+      framePossibilitiesF = [i[i.find("M"):] for i in trans.split("*") if "M" in i]
+      allPossibilities += framePossibilitiesF
+  allPossibilitiesLengths = [len(i) for i in allPossibilities]
+
+  if len(allPossibilitiesLengths) == 0:
+      raise Exception("no candidate ORFs")
+
+  proteinAsString = allPossibilities[allPossibilitiesLengths.index(max(allPossibilitiesLengths))]
+
+  return Seq(proteinAsString, alphabet=ProteinAlphabet)
+"""
+
 
 def complement(s):
     """relationship between two structures"""
@@ -43,14 +65,19 @@ AT = (1 - frequency / 100) / 2
 """Probability. Rule of molecular biology"""
 P = [AT, CG, AT, CG]
 
+
+
+find_largest_polypeptide_in_DNA(random_dna_sequence(length))
 seq = Seq(random_dna_sequence(length))
+print("ASDADSADSADSADSADASDSADSADASDSAD")
+startP = re.compile('ATG')
 max_len = int(0);
 for strand, nuc in [(1, seq), (-1, seq.reverse_complement())]:
     for frame in range(3):
         for pro in nuc[frame:].translate(TABLE).split("*"):
             if len(pro) >= max_len:
                 max_len = len(pro)
-                RESULTS.append([pro[:30], pro[-3:], len(pro), strand, frame])
+                RESULTS = [pro[:30], pro[-3:], len(pro), strand, frame]
                 # print("%s...%s - length %i, strand %i, frame %i" % (pro[:30], pro[-3:], len(pro), strand, frame))
 print(seq)
 print(*RESULTS)
